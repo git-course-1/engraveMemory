47d54d7bf488359558f326674320c2c139363039
Ilya Efimov <efimilya@gmail.com>
2020-02-24 13:08:01 +0300

    добавлен компонент для хранения навигации в IoC-контейнере

---
+++ EngraveMemory/Common/RootPageNavigation.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+ 
+ namespace EngraveMemory.Common
+ {
+     public class RootPageNavigation
+     {
+         public INavigation Navigation { get; private set; }
+ 
+         public void SetNavigation(INavigation navigation)
+         {
+             Navigation = navigation;
+         }
+ 
+     }
+ }
