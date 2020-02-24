e53d7fc00c76f483a943242bfe5d1cb1d18fca98
Ilya Efimov <efimilya@gmail.com>
2020-02-24 13:09:49 +0300

    добавлена регистрация в контейнере детального элемента списка

---
+++ EngraveMemory/MemorialDetails/EditMemorialRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Autofac;
+using EngraveMemory.Common;
+using EngraveMemory.Engine;
+
+namespace EngraveMemory.MemorialDetails
+{
+    public static class EditMemorialRegistration
+    {
+        public static void RegisterEditMemorial(this ContainerBuilder builder)
+        {
+            DevsTeam.Framework.Autofac.FactoryRegistration.RegisterFactory<MemorialDetailsVm, Memorial>(builder,
+                (childBuilder, m) =>
+                {
+                    childBuilder.RegisterType<MemorialDetailsVm>().SingleInstance();
+                    childBuilder.Register<Memorial>(c => m);
+                });
+            
+//           
+        }
+    }
+}
