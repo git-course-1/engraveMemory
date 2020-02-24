6192163479913b5b04a8ad6205e1f4ae78a746ab
Kirill Maurin <leo.bonart@gmail.com>
2020-02-24 22:18:09 +0300

    Add item list page

---
+++ EngraveMemory/MemorialList/MemorialView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace EngraveMemory.MemorialList
+{
+    public partial class MemorialView : ContentView
+    {
+        public MemorialView() => InitializeComponent();
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            var b = BindingContext;
+        }
+    }
+}
