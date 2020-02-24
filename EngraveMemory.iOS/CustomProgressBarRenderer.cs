f937b3f6df29f0c3b7c99e7b0bd24a0da6d0c216
Ilya Efimov <efimilya@gmail.com>
2020-02-24 13:12:45 +0300

    добавлен кастомный ProgressBar

---
+++ EngraveMemory.iOS/CustomProgressBarRenderer.cs
@@ -0,0 +1,32 @@
+﻿using CoreGraphics;
+using EngraveMemory.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+//[assembly: ExportRenderer(typeof(ProgressBar), typeof(CustomProgressBarRenderer))]
+namespace EngraveMemory.iOS
+{
+    public class CustomProgressBarRenderer : ProgressBarRenderer
+    {
+        protected override void OnElementChanged(
+            ElementChangedEventArgs<Xamarin.Forms.ProgressBar> e)
+        {
+            base.OnElementChanged(e);
+
+//            Control.ProgressTintColor = Color.FromRgb(182, 231, 233).ToUIColor();// If you want to change the color
+
+        }
+
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            var X = 1.0f;
+            var Y = 10.0f; //This changes the height
+
+            CGAffineTransform transform = CGAffineTransform.MakeScale(X, Y);
+            this.Transform = transform;
+        }
+    }
+}
