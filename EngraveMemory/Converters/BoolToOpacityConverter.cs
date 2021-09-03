b781138cc095cf2a471f9c25263077b8b647c988
Arthur Chaikovski <arthur.chaykovski@gmail.com>
2021-09-03 22:31:03 +0300

    #1 Добавлены конвертеры для кастомного отображения прогресс бара

---
+++ EngraveMemory/Converters/BoolToOpacityConverter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace EngraveMemory.Converters
+{
+    public class BoolToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? 1 : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EngraveMemory/Converters/NullToVisiblityConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace EngraveMemory.Converters
+{
+    public class NullToVisiblityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
